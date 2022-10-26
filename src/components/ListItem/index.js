7464d5358938c634cced2bf805fd2b60c53a7c2e
Meena Forderer <meena.foerderer@gmail.com>
2022-10-26 15:16:54 +0200

    add check todos functionality

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,9 +1,19 @@
-import { ListItem as TestItem } from "./ListItem.styled";
+import { ListItem as Item } from "./ListItem.styled";
+import Checkbox from "@mui/material/checkbox";
 
-export default function ListItem({ todo }) {
+export default function ListItem({ todo, toggleCheckbox }) {
   return (
     <>
-      <TestItem todo={todo}>{todo.title}</TestItem>
+      <Checkbox
+        key={`check${todo.id}`}
+        checked={todo.isChecked}
+        onClick={() => {
+          toggleCheckbox(todo.id);
+        }}
+      />
+      <Item key={todo.id} todo={todo}>
+        {todo.title}
+      </Item>
     </>
   );
 }
